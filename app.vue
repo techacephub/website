c18aa27af4d074e672366486e7f1a5adbc18a78d
cradoe <noblewapinfo@gmail.com>
2022-11-15 15:29:51 +0100

    Created hero section

--- app.vue
+++ app.vue
@@ -2,6 +2,11 @@
     <div>
         <WhatWeHaveDone />
     </div>
+  <div>
+    <!-- This section to mimic navbar  -->
+    <div class="h-16 w-full bg-red-500"></div>
+    <HeroSection />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
