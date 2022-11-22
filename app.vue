c77590184f5ac53a2a31c452876573a4946ffd59
haayzeed <dosunmuafeez37@gmail.com>
2022-11-23 00:07:21 +0100

    about and footer completed

--- app.vue
+++ app.vue
@@ -6,6 +6,9 @@
     <!-- This section to mimic navbar  -->
     <div class="h-16 w-full bg-red-500"></div>
     <HeroSection />
+    <Header />
+    <AboutUs />
+    <Footer />
   </div>
 </template>
 
---
+++ components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="bg-[#18309F]">
+        <div class="container mx-auto pt-[50px] py-[20px] flex px-5 md:px-20">
+            <div class="w-[25%]">
+                <h4 class="text-white mb-[20px] font-semibold text-[20px]">Quick Links</h4>
+                <ul class="text-white text-[14px]">
+                    <li class="mb-[5px]">
+                        <a href="">Who we are</a>
+                    </li>
+                    <li class="mb-[5px]">
+                        <a href="">Events</a>
+                    </li>
+                    <li class="mb-[5px]">
+                        <a href="">Team</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="w-[25%]">
+                <h4 class="text-white mb-[20px] font-semibold text-[20px]">Follow Us</h4>
+                <ul class="text-white text-[14px]">
+                    <li class="mb-[5px]">
+                        <a href="">Facebook</a>
+                    </li>
+                    <li class="mb-[5px]">
+                        <a href="">Instagram</a>
+                    </li>
+                    <li class="mb-[5px]">
+                        <a href="">Twitter</a>
+                    </li>
+                    <li class="mb-[5px]">
+                        <a href="">Linkedin</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="w-[50%]">
+                <label class="text-white text-[14px]" for="">Join our newsletters to stay up to date</label>
+                <div class="flex gap-4">
+                    <input
+                    type="text"
+                    class="w-full h-10 text-gray-800 text-[14px]  px-4 focus:ring-0"
+                    placeholder="Enter email address"
+                    />
+                    <button class="bg-[#4FB400] font-semibold text-white h-10 px-[20px]">Subscribe</button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container mx-auto py-[30px] px-5 md:px-20">
+            <hr class="border-t border-white w-full">
+            <p class="text-white text-[12px] text-center mt-[30px]">(c) 2022, Programmers’ Hub | All rights reserved</p>
+        </div>
+    </div>
+</template>
